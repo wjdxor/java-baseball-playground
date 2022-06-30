4d7311de1974097787e4dc43c341808d326f1179
JT LIM <wjdxortaek@gmail.com>
2022-07-01 03:28:04 +0900

    StringTest 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,42 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        String[] actual2 = "1".split(",");
+        assertThat(actual).contains("2");
+        assertThat(actual2).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생한다.")
+    void charAt() {
+        assertThat("abc".charAt(1)).isEqualTo('b');
+        assertThatThrownBy(()->{
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: %d", 3);
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(()->{
+                    "abc".charAt(5);
+                }).withMessageContaining("String index out of range: %d", 5);
     }
 }
